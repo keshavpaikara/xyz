12794d2d72bbbefaf6c38461c4d6104b08771507
keshavpaikara <keshavpaikara@gmail.com>
2022-07-23 14:58:39 +0530

    intializing pushing  to github for the first time

---
+++ app/Http/Controllers/xyzcontroller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class xyzcontroller extends Controller
+{
+    function show(){return "xyzcontroller is ready";}
+    
+}
